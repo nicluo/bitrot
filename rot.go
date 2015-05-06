dd4edc934145c6f60c3ea73d72ea9bbc8ac90163
Nicholas Luo <nicholasluo@gmail.com>
2015-05-06 09:13:33 +0800

    Use ioutil.Readfile instead of os.Open

--- rot.go
+++ rot.go
@@ -23,16 +23,12 @@ func check(e error) {
 func rot(testfile string) {
   testdir := ("./photos/")
 
-  f, err := os.Open(testdir + testfile)
+  b1, err := ioutil.ReadFile(testdir + testfile)
   check(err)
 
-  info, err := f.Stat()
-  check(err)
+  n1 := len(b1)
 
-  b1 := make([]byte, info.Size())
-  f.Read(b1)
-
-  for i := 0; i < int(info.Size() / 1000); i++ {
+  for i := 0; i < int(n1 / 1000); i++ {
     filename := fmt.Sprintf("./out/%d%s", i, filepath.Ext(testfile))
     w, err := os.Create(filename)
     check(err)
@@ -51,19 +47,10 @@ func rot(testfile string) {
 }
 
 func checksum(path string){
-  f, err := os.Open(path)
+  b1, err := ioutil.ReadFile(path)
   check(err)
 
-  info, err := f.Stat()
-  check(err)
-
-  b1 := make([]byte, info.Size())
-  n1, err := f.Read(b1)
-  check(err)
-
-  err = f.Close()
-  check(err)
-
+  n1 := len(b1)
   n2 := crc32.ChecksumIEEE(b1)
   fmt.Printf("%s\n%d bytes: %d\n", path, n1, n2)
 }
